a4b24407cab832c4365c8737bd1a95fb7e262a49
steel5rat <ivan@ubuntu.(none)>
2013-06-18 14:56:29 +0300

    empty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Peg::Application.config.secret_token = '54b5254e8dac4b78080987510a1e6aad0327c43d3ecb12520b44966469f2488c6228e32e75bd434e16e9e256f61acacf666c802629df2a550e0a0842fba83fab'
